582a7bdc317227719606ec9381771cf9d84b303f
zakymuhamad <arzaki666@gmail.com>
2023-08-25 10:03:03 +0700

    test2

--- resources/views/pages/support_team/dorms/edit.blade.php
+++ resources/views/pages/support_team/dorms/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('page_title', 'Edit Dorm - '.$dorm->name)
+@section('page_title', 'Edit Dorm - ' . $dorm->name)
 @section('content')
 
     <div class="card">
@@ -11,24 +11,29 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">
-                    <form class="ajax-update" data-reload="#page-header" method="post" action="{{ route('dorms.update', $dorm->id) }}">
+                    <form class="ajax-update" data-reload="#page-header" method="post"
+                        action="{{ route('dorms.update', $dorm->id) }}">
                         @csrf @method('PUT')
                         <div class="form-group row">
-                            <label class="col-lg-3 col-form-label font-weight-semibold">Name <span class="text-danger">*</span></label>
+                            <label class="col-lg-3 col-form-label font-weight-semibold">Name <span
+                                    class="text-danger">*</span></label>
                             <div class="col-lg-9">
-                                <input name="name" value="{{ $dorm->name }}" required type="text" class="form-control" placeholder="Name of Dormitory">
+                                <input name="name" value="{{ $dorm->name }}" required type="text"
+                                    class="form-control" placeholder="Name of Dormitory">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label class="col-lg-3 col-form-label font-weight-semibold">Description</label>
                             <div class="col-lg-9">
-                                <input name="description" value="{{ $dorm->description }}"  type="text" class="form-control" placeholder="Description of Dormitory">
+                                <input name="description" value="{{ $dorm->description }}" type="text"
+                                    class="form-control" placeholder="Description of Dormitory">
                             </div>
                         </div>
 
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
+                            <button type="submit" class="btn btn-primary">Submit form <i
+                                    class="icon-paperplane ml-2"></i></button>
                         </div>
                     </form>
                 </div>
@@ -36,6 +41,6 @@
         </div>
     </div>
 
-    {{--Class Edit Ends--}}
+    {{-- Class Edit Ends --}}
 
 @endsection
--- resources/views/pages/support_team/sections/edit.blade.php
+++ resources/views/pages/support_team/sections/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('page_title', 'Edit Section of '.$s->my_class->name)
+@section('page_title', 'Edit Section of ' . $s->my_class->name)
 @section('content')
 
     <div class="card">
@@ -14,33 +14,39 @@
                     <form class="ajax-update" method="post" action="{{ route('sections.update', $s->id) }}">
                         @csrf @method('PUT')
                         <div class="form-group row">
-                            <label class="col-lg-3 col-form-label font-weight-semibold">Name <span class="text-danger">*</span></label>
+                            <label class="col-lg-3 col-form-label font-weight-semibold">Name <span
+                                    class="text-danger">*</span></label>
                             <div class="col-lg-9">
-                                <input name="name" value="{{ $s->name }}" required type="text" class="form-control" placeholder="Name of Class">
+                                <input name="name" value="{{ $s->name }}" required type="text"
+                                    class="form-control" placeholder="Name of Class">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="my_class_id" class="col-lg-3 col-form-label font-weight-semibold">Class </label>
                             <div class="col-lg-9">
-                                <input class="form-control" id="my_class_id" disabled="disabled" type="text" value="{{ $s->my_class->name }}">
+                                <input class="form-control" id="my_class_id" disabled="disabled" type="text"
+                                    value="{{ $s->my_class->name }}">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="teacher_id" class="col-lg-3 col-form-label font-weight-semibold">Teacher</label>
+                            <label for="teacher_id" class="col-lg-3 col-form-label font-weight-semibold">Guru</label>
                             <div class="col-lg-9">
-                                <select data-placeholder="Select Teacher" class="form-control select-search" name="teacher_id" id="teacher_id">
+                                <select data-placeholder="Select Teacher" class="form-control select-search"
+                                    name="teacher_id" id="teacher_id">
                                     <option value=""></option>
-                                    @foreach($teachers as $t)
-                                        <option {{ $s->teacher_id == $t->id ? 'selected' : '' }} value="{{ Qs::hash($t->id) }}">{{ $t->name }}</option>
+                                    @foreach ($teachers as $t)
+                                        <option {{ $s->teacher_id == $t->id ? 'selected' : '' }}
+                                            value="{{ Qs::hash($t->id) }}">{{ $t->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
 
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
+                            <button type="submit" class="btn btn-primary">Submit form <i
+                                    class="icon-paperplane ml-2"></i></button>
                         </div>
                     </form>
                 </div>
@@ -48,6 +54,6 @@
         </div>
     </div>
 
-    {{--Section Edit Ends--}}
+    {{-- Section Edit Ends --}}
 
 @endsection
